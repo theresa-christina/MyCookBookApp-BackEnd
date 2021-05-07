e2aadb484fc1332fe3f90242ec8bd8931b7c1a5e
theresa-mashura <theresamashura@gmail.com>
2021-05-07 19:29:55 -0400

    did photo storage classes

--- src/main/java/com/passionproject/cookbook/controllers/PhotoStorageController.java
+++ src/main/java/com/passionproject/cookbook/controllers/PhotoStorageController.java
@@ -1,12 +1,15 @@
 package com.passionproject.cookbook.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.passionproject.cookbook.services.PhotoStorageService;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@RestController
+@RequestMapping("/photo-storage")
 @CrossOrigin
 public class PhotoStorageController {
 
@@ -17,4 +20,27 @@ public class PhotoStorageController {
         this.svc = svc;
     }
 
+    @PostMapping("/upload")
+    public ResponseEntity<Long> uploadPhoto(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(this.svc.uploadPhoto(file), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{filename}")
+    public ResponseEntity<Long> deleteFile (@PathVariable String filename) {
+        return new ResponseEntity<>(this.svc.deleteFile(filename), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<ByteArrayResource> getPhotoByName(@PathVariable String filename) {
+        byte[] data = this.svc.downloadPhotoByName(filename);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<ByteArrayResource> getPhotoByKey(@PathVariable String keyName) {
+        byte[] data = this.svc.downloadPhotoByKey(keyName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
 }
--- src/main/java/com/passionproject/cookbook/services/PhotoStorageService.java
+++ src/main/java/com/passionproject/cookbook/services/PhotoStorageService.java
@@ -1,7 +1,90 @@
 package com.passionproject.cookbook.services;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
+import com.passionproject.cookbook.models.Photo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Service
+@Slf4j
 public class PhotoStorageService {
+
+    @Value("${application.bucket.name}")
+    private String bucketName;
+    private AmazonS3 s3Client;
+    private PhotoDataService svc;
+
+    @Autowired
+    public PhotoStorageService(AmazonS3 s3Client, PhotoDataService svc) {
+        this.s3Client = s3Client;
+        this.svc = svc;
+    }
+
+    // Add Photo in RDS Table THEN add Photo to S3
+    public Long uploadPhoto(MultipartFile file) {
+        Long photoId = svc.addPhoto(new Photo()).getId();
+
+        File fileObj = convertMultiPartFileToFile(file);
+        String filename = photoId.toString();
+        s3Client.putObject(
+                new PutObjectRequest(bucketName, filename, fileObj)
+        );
+        fileObj.delete(); // delete so it doesn't keep adding it
+        return photoId;
+    }
+
+    // Use %20 for spaces in filenames when testing via Insomnia, Postman, etc.
+    // Delete from AWS S3 and from VideoDataRepository
+    public Long deleteFile(String filename) {
+        s3Client.deleteObject(bucketName, filename);
+        Long photoId = Long.parseLong(filename);
+        svc.deletePhoto(photoId);
+        return photoId;
+    }
+
+    public byte[] downloadPhotoByName(String filename) {
+        S3Object s3Object = s3Client.getObject(bucketName, filename);
+        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+        try {
+            byte[] content = IOUtils.toByteArray(inputStream);
+            return content;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public byte[] downloadPhotoByKey(String keyName) {
+        GetObjectRequest req = new GetObjectRequest(this.bucketName, keyName);
+        S3Object s3Object = s3Client.getObject(req);
+        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+        try {
+            byte[] content = IOUtils.toByteArray(inputStream);
+            return content;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            log.error("Error converting multipartFile to file", e);
+        }
+
+        return convertedFile;
+    }
 }
